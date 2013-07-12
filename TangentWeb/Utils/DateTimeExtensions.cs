e228738c9f8d86e28ddab5850ea69554d62c50b2
Stuart Bray <stu.bray@gmail.com>
2013-07-12 00:45:14 -0500

    Some polish to single tangent view page

---
+++ TangentWeb/Utils/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TangentWeb.Utils
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static double GetEpochTicks(this DateTime dateTime)
+        {
+            return dateTime.Subtract(Epoch).TotalMilliseconds;
+        }
+    }
+}
